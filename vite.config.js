a9af796a1b2aceebc6a482e7f3bacd6711ae7644
Soulayman Elkharraz <soulaymanelkharraz2006@gmail.com>
2026-03-10 14:03:17 +0000

    add all

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // <--- ADD THIS LINE! This makes sure your CSS/JS links work.
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
